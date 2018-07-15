626d3fbf8544dc2e3d375f331d4cf56e265d44d1
Mohit Sharma <msvbs98@gmail.com>
2018-07-15 21:41:28 +0530

    Add Create Project Route

--- src/units/http-kernel/route-definitions.js
+++ src/units/http-kernel/route-definitions.js
@@ -1,5 +1,6 @@
 import { routes as core } from '../core'
 import { routes as auth } from '../auth'
 import { routes as contributions } from '../contributions'
+import { routes as projects } from '../projects'
 
-export default [core, auth, contributions]
+export default [core, auth, contributions, projects]
