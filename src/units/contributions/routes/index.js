7892dcce16810d72ab1bcf85392ef92ef74a00f0
Mohit Sharma <msvbs98@gmail.com>
2018-07-23 17:16:05 +0530

    Add getContributions endpoint

--- src/units/contributions/routes/index.js
+++ src/units/contributions/routes/index.js
@@ -1,6 +1,7 @@
 import saveContribution from './saveContribution'
+import getContributions from './getContributions'
 
 export default {
   prefix: '/contributions',
-  routes: [saveContribution]
+  routes: [saveContribution, getContributions]
 }
