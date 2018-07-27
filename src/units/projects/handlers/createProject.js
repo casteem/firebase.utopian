5c71be758e0cb09a3b2786497a80ae5666d9c936
Mohit Sharma <msvbs98@gmail.com>
2018-07-27 21:17:27 +0530

    Finish Github Validation

--- src/units/projects/handlers/createProject.js
+++ src/units/projects/handlers/createProject.js
@@ -7,7 +7,7 @@ import { get } from 'lodash'
 // custom https errors.
 import { HttpsError } from 'src/support/firebase/functions/handler/errors'
 
-import * as Github from '@octokit/rest'
+const octokit = require('@octokit/rest')()
 
 /**
  *
@@ -35,7 +35,7 @@ export const handler = async (data, context) => {
     .then(snapshot => {
       console.log(snapshot)
       snapshot.forEach(doc => {
-        docs.push({id: doc.id})
+        docs.push({id: doc.id, data: doc.data()})
       })
     })
 
@@ -43,20 +43,25 @@ export const handler = async (data, context) => {
   const match = new RegExp(data.creator, 'i')
   docs = docs.filter((doc) => match.test(doc.id))
   console.log(docs)
-  const username = docs.github.username
+  const username = docs[0].data.github.username
   console.log(username)
 
   if (data.openSource && data.platforms && data.platforms.github.token) {
     const token = data.platforms.github.token
     console.log(token)
-    await Github.authenticate({
+    await octokit.authenticate({
       type: 'token',
       token
     })
     const splittedGithubRepository = data.platforms.github.repository.split('/')
     const repo = splittedGithubRepository.pop()
     const owner = splittedGithubRepository.pop()
-    const result = (await Github.repos.reviewUserPermissionLevel({ owner, repo, username })).data.permission
+    let result
+    try {
+      result = (await octokit.repos.reviewUserPermissionLevel({ owner, repo, username })).data.permission
+    } catch (err) {
+      result = false
+    }
 
     if (!(result === 'admin')) {
       return Promise.reject(new HttpsError(
@@ -66,7 +71,7 @@ export const handler = async (data, context) => {
     }
     delete data.platforms.github.token
   }
-
+  console.log(data)
   if (!data.images || !data.description || !data.name || !data.creator || !data.tags || !((data.openSource && data.platforms) || !data.openSource) || !data.slug || !data.id) {
     return Promise.reject(new HttpsError(
       'failed-precondition',
